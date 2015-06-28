d52cc143f0c704e51d9785e907ab21c072de9091
rootbin <rootbin@rootbin.rootbin.net>
2015-06-27 22:00:28 -0300

    Tabs to spaces

--- config/app.php
+++ config/app.php
@@ -13,45 +13,45 @@ return new \Phalcon\Config([
      * General API's configurations
      **/
     'api' => [
-		/**
-	     * Enable or disable Cross-site HTTP requests
-	     **/
-	    'cors' => [
-			'enabled' => true,
-			'hosts' => '*'
-	    ],
+        /**
+         * Enable or disable Cross-site HTTP requests
+         **/
+        'cors' => [
+            'enabled' => true,
+            'hosts' => '*'
+        ],
 
-	    /**
-	     * API's headers
-	     *
-	     * By default the application add:
-	     *     - Content-Type: application/json
-	     *     - Access-Control-Allow-Origin: * (if cors is enabled).
-	     *     - Access-Control-Allow-Methods: GET, POST, PUT, DELETE
-	     *
-	     * If you want to overwrite any header, this is your section.
-	     *
-	     **/
-	    'headers' => [
-	    	'Content-Type' => 'application/json',
-	    	'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE'
-	    ],
+        /**
+         * API's headers
+         *
+         * By default the application add:
+         *     - Content-Type: application/json
+         *     - Access-Control-Allow-Origin: * (if cors is enabled).
+         *     - Access-Control-Allow-Methods: GET, POST, PUT, DELETE
+         *
+         * If you want to overwrite any header, this is your section.
+         *
+         **/
+        'headers' => [
+            'Content-Type' => 'application/json',
+            'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE'
+        ],
     ],
 
     /**
      * JSON response configurations
      **/
     'json' => [
-	    /**
-	     * JSON encode options.
-	     * 
-	     * JSON_PRETTY_PRINT option is pre-seted if debig is enabled.
-	     * 
-	     * More information: http://php.net/manual/en/json.constants.php
-	     **/
-    	'options' => [
-	    	JSON_NUMERIC_CHECK
-    	],
-    	'depth' => 512
+        /**
+         * JSON encode options.
+         * 
+         * JSON_PRETTY_PRINT option is pre-seted if debig is enabled.
+         * 
+         * More information: http://php.net/manual/en/json.constants.php
+         **/
+        'options' => [
+            JSON_NUMERIC_CHECK
+        ],
+        'depth' => 512
     ]
 ]);
